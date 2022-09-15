d10c6e006c7a5bd3e58d9d932e5416e68f85623a
Samuel Adesoga <samueladesoga@gmail.com>
2022-09-15 08:22:59 +0100

    fixed sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-    siteUrl: process.env.SITE_URL || 'https://valuehut.co',
+    siteUrl: process.env.SITE_URL || 'https://www.valuehut.co',
     generateRobotsTxt: true, // (optional)
     // ...other options
 }
