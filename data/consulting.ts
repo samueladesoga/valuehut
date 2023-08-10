214cb8c466482588c90b432aaf98bdcf739f0e76
Samuel Adesoga <samueladesoga@gmail.com>
2023-08-10 22:52:45 +0100

    fixed consulting page

--- data/consulting.ts
+++ data/consulting.ts
@@ -34,15 +34,16 @@ export const consulting: ConsultingObject = {
         imageURL: '/images/mikael.jpg',
         articles: [
             {
-                title: 'Continuous Integration / Continous Delivery',
+                title: 'Continous Delivery',
                 content: `A central theme to application delivery is enabling teams to build better products faster. 
-                Continuous Integration and Continuous Delivery are practices that enables the product teams gather feedback in near real time.
-                CI / CD is underpinned by making information transparent and automating collection of metrics about the product.
-                Our consultants have implemented and supported many organisations with their CI/CD Implementation. Our typical process would involve:`,
+                Continuous Integration and Continuous Delivery are practices that enables the product teams gather feedback in near real time as the product is being built.
+                Continuous Delivery is a set of practices that provide to capabilities required to build, test, package and deploy application code into an environment; information such
+                as quality of the product, measures of technical debt is collect and displayed in report format that are easy to understand for entire product teams. 
+                Our consultants have implemented and supported many organisations with their Continuous Delivery Implementation. Our typical process would involve:`,
                 contentList: [
                     `An evaluation of the current state including tools / technology being used`,
-                    `Design a strategy for CI/CD including covering any gaps in automated checks`,
-                    `Tools selection for CI/CD`,
+                    `Design a strategy for Continuous Delivery including covering any gaps in automated checks`,
+                    `Tools selection for Continuous Delivery`,
                     `Coach Product team on Agile Testing Mindset and practices`,
                     `Post Implementation support`,
                 ],
@@ -52,14 +53,37 @@ export const consulting: ConsultingObject = {
                 // },
             },
             {
-                title: 'Behaviour Driven Development',
-                content: `Behaviour Driven Development is an Agile Development practice that fosters collaboration between 
-                the Business and the Development teams. It encourages communication based on the expected behaviour of the 
-                product to be implemented and subsequently enables the team to capture requirements as specification which 
-                can then be automated by the Development Team.`,
+                title: 'Test Automation',
+                content: `Organisation that care about faster delivery, lowering time-to-market and improving the overall quality of their product have to invest into automated
+                testing capabilities. Our consultants and coaches have helped many organisation eliminate extended manual tests as part of their product delivery cycles.
+                Every form of testing can be automated from Functional Tests, Unit Tests, Performance Tests and additional Non-Functional Tests using matured tools and integrating these
+                tests into the continuous delivery pipelines. At ValueHut, our consultants are able to: `,
+                contentList: [
+                    `Train your product teams on approaches to Software Testing`,
+                    `Augument your product teams with our people; we work along side your product team improving the quality of the product`,
+                    `Implement a thorough automated test suite for existing or new products`,
+                    `Design a Test Automation strategy for products across your organisation`,
+                ],
+                // link: {
+                //     label: 'This is a test link',
+                //     url: 'https://google.com',
+                // },
             },
             {
-                title: 'Agile Fundamentals',
+                title: 'Product Backlog Management',
+                content: `Product Backlog Managemment is a set of Agile Practices that helps organisations structure their product backlog from idea to delivery of product to users.
+                These practices are applicable to old product backlog that might seemingly out of control and new product backlogs for new initiatives. Some of the concerns that we help
+                address as part of this consulting service include:`,
+
+                contentList: [
+                    `Co-creation of product goals for products in your organisation using OKRs`,
+                    `Help define a process for ordering product backlog items in the product backlog`,
+                    `Design a product framework for effective collaboration with multiple stakeholders across the organisation`,
+                    `Metrics that help the product teams show progress across their product backlog items.`,
+                ],
+            },
+            {
+                title: 'Agile Fundamentals Training',
                 content: `Our consultants are able to teach and mentor your teams as you adopt any of the Agile Frameworks such as Scrum.
                 We help your teams improve on the use of Frameworks such as Scrum and Kanban by introducing complimentary practices.
                 Some of the complimentary practices that we support your teams with are listed below.
