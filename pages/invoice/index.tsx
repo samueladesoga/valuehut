a8ee6a1ddc437e5a39bba051f3c2aa81f4113f38
Ahmed <zrouqui.ahmed.az@gmail.com>
2024-03-10 12:31:20 +0000

    fix: add styles

--- pages/invoice/index.tsx
+++ pages/invoice/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { InvoiceSchemaType } from '../../lib/schemas/invoice.schema'
 import { useForm, SubmitHandler } from 'react-hook-form'
-import { Button } from '@mui/material'
+import { Button, Input } from '@nextui-org/react'
 
 function InvoicePage() {
     const {
@@ -16,15 +16,51 @@ function InvoicePage() {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register('firstName', { required: true })} />
-            <input {...register('lastName', { required: true })} />
-            <input {...register('email', { required: true })} />
-            <input {...register('phoneNumber', { required: true })} />
-            <input {...register('address', { required: true })} />
+        <div className="max-w-[400px] flex items-center m-auto min-h-[1000px]">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="flex w-full flex-wrap md:flex-nowrap gap-2 mb-4">
+                    <Input
+                        label="First name"
+                        fullWidth
+                        {...register('firstName', { required: true })}
+                        className={`${errors?.firstName?.message ? '!outline-red-400' : ''}`}
+                    />
+                    <Input
+                        label="Last name"
+                        fullWidth
+                        {...register('lastName', { required: true })}
+                        className={`${errors?.lastName?.message ? '!outline-red-400' : ''}`}
+                    />
+                </div>
+                <div className="flex w-full flex-wrap md:flex-nowrap gap-2 mb-4">
+                    <Input
+                        type="email"
+                        label="Email"
+                        fullWidth
+                        {...register('email', { required: true })}
+                        className={`${errors?.email?.message ? '!outline-red-400' : ''}`}
+                    />
+                    <Input
+                        label="Phone Number"
+                        fullWidth
+                        {...register('phoneNumber', { required: true })}
+                        className={`${errors.phoneNumber ? 'border-red-400' : ''}`}
+                    />
+                </div>
+                <div className="flex w-full flex-wrap md:flex-nowrap gap-2 mb-4">
+                    <Input
+                        label="Address"
+                        fullWidth
+                        {...register('address', { required: true })}
+                        className={`${errors.address ? 'border-red-400' : ''}`}
+                    />
+                </div>
 
-            <Button type="submit">Send me an invoice</Button>
-        </form>
+                <Button type="submit" fullWidth variant="bordered">
+                    Send me an invoice
+                </Button>
+            </form>
+        </div>
     )
 }
 
